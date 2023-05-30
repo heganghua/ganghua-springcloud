36fa8adba7366d878250752658b085ae6bc47132
heganghua <heganghua1113@163.com>
2023-05-30 23:27:52 +0800

    new idea first commit at home

--- ganghua-gateway/gateway-admin/src/main/java/xyz/ganghua/gateway/admin/controller/GatewayRouteController.java
+++ ganghua-gateway/gateway-admin/src/main/java/xyz/ganghua/gateway/admin/controller/GatewayRouteController.java
@@ -3,14 +3,9 @@ package xyz.ganghua.gateway.admin.controller;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import xyz.ganghua.entity.vo.Result;
-import xyz.ganghua.gateway.admin.controller.form.GatewayRouteForm;
 import xyz.ganghua.gateway.admin.service.IGatewayRouteService;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/gateway/routes")
@@ -21,9 +16,9 @@ public class GatewayRouteController {
     @Autowired
     private IGatewayRouteService gatewayRouteService;
 
-    public Result add(@Valid @RequestBody GatewayRouteForm gatewayRoutForm) {
-
-
-    }
+//    public Result add(@Valid @RequestBody GatewayRouteForm gatewayRoutForm) {
+//
+//
+//    }
 
 }
